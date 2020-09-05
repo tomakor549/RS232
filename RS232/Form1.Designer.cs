8d49d3bbaa052a220084f765fb4e8f8f375cbc16
tomakor549 <32541866+tomakor549@users.noreply.github.com>
2020-09-05 21:57:39 +0200

    Dodanie formatu znakowego i wyboru terminatora
    Sortowanie świruje, porawa drobnych błędów

--- RS232/Form1.Designer.cs
+++ RS232/Form1.Designer.cs
@@ -42,7 +42,24 @@
             this.txtReceive = new System.Windows.Forms.RichTextBox();
             this.buttonSend = new System.Windows.Forms.Button();
             this.buttonReceive = new System.Windows.Forms.Button();
+            this.comboBoxDataBits = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxParityBits = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBoxStopBits = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.checkBoxCR = new System.Windows.Forms.CheckBox();
+            this.checkBoxLF = new System.Windows.Forms.CheckBox();
+            this.textBoxTerminatorManualy = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
             // 
             // label1
             // 
@@ -56,7 +73,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(564, 150);
+            this.label2.Location = new System.Drawing.Point(564, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 1;
@@ -82,7 +99,7 @@
             // 
             // buttonConnect
             // 
-            this.buttonConnect.Location = new System.Drawing.Point(567, 94);
+            this.buttonConnect.Location = new System.Drawing.Point(567, 277);
             this.buttonConnect.Name = "buttonConnect";
             this.buttonConnect.Size = new System.Drawing.Size(75, 23);
             this.buttonConnect.TabIndex = 6;
@@ -92,7 +109,7 @@
             // 
             // buttonDisconnect
             // 
-            this.buttonDisconnect.Location = new System.Drawing.Point(648, 94);
+            this.buttonDisconnect.Location = new System.Drawing.Point(656, 277);
             this.buttonDisconnect.Name = "buttonDisconnect";
             this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
             this.buttonDisconnect.TabIndex = 7;
@@ -105,16 +122,17 @@
             this.comboBoxPort.FormattingEnabled = true;
             this.comboBoxPort.Location = new System.Drawing.Point(567, 67);
             this.comboBoxPort.Name = "comboBoxPort";
-            this.comboBoxPort.Size = new System.Drawing.Size(156, 21);
+            this.comboBoxPort.Size = new System.Drawing.Size(175, 21);
             this.comboBoxPort.TabIndex = 8;
             // 
             // comboBoxSpeed
             // 
             this.comboBoxSpeed.FormattingEnabled = true;
-            this.comboBoxSpeed.Location = new System.Drawing.Point(567, 166);
+            this.comboBoxSpeed.Location = new System.Drawing.Point(567, 107);
             this.comboBoxSpeed.Name = "comboBoxSpeed";
-            this.comboBoxSpeed.Size = new System.Drawing.Size(156, 21);
+            this.comboBoxSpeed.Size = new System.Drawing.Size(175, 21);
             this.comboBoxSpeed.TabIndex = 9;
+            this.comboBoxSpeed.SelectedIndexChanged += new System.EventHandler(this.comboBoxSpeed_SelectedIndexChanged);
             // 
             // txtMessage
             // 
@@ -152,11 +170,137 @@
             this.buttonReceive.UseVisualStyleBackColor = true;
             this.buttonReceive.Click += new System.EventHandler(this.buttonReceive_Click);
             // 
+            // comboBoxDataBits
+            // 
+            this.comboBoxDataBits.FormattingEnabled = true;
+            this.comboBoxDataBits.Items.AddRange(new object[] {
+            "7",
+            "8"});
+            this.comboBoxDataBits.Location = new System.Drawing.Point(656, 199);
+            this.comboBoxDataBits.Name = "comboBoxDataBits";
+            this.comboBoxDataBits.Size = new System.Drawing.Size(87, 21);
+            this.comboBoxDataBits.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(653, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Bity pola danych";
+            // 
+            // comboBoxParityBits
+            // 
+            this.comboBoxParityBits.FormattingEnabled = true;
+            this.comboBoxParityBits.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even"});
+            this.comboBoxParityBits.Location = new System.Drawing.Point(567, 199);
+            this.comboBoxParityBits.Name = "comboBoxParityBits";
+            this.comboBoxParityBits.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxParityBits.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.3F);
+            this.label4.Location = new System.Drawing.Point(599, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 19);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Format znaku:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(564, 183);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Kontrola";
+            // 
+            // comboBoxStopBits
+            // 
+            this.comboBoxStopBits.FormattingEnabled = true;
+            this.comboBoxStopBits.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.comboBoxStopBits.Location = new System.Drawing.Point(567, 239);
+            this.comboBoxStopBits.Name = "comboBoxStopBits";
+            this.comboBoxStopBits.Size = new System.Drawing.Size(175, 21);
+            this.comboBoxStopBits.TabIndex = 20;
+            this.comboBoxStopBits.SelectedIndexChanged += new System.EventHandler(this.comboBoxStopBits_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(564, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Bity stopu";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // checkBoxCR
+            // 
+            this.checkBoxCR.AutoSize = true;
+            this.checkBoxCR.Location = new System.Drawing.Point(569, 326);
+            this.checkBoxCR.Name = "checkBoxCR";
+            this.checkBoxCR.Size = new System.Drawing.Size(41, 17);
+            this.checkBoxCR.TabIndex = 22;
+            this.checkBoxCR.Text = "CR";
+            this.checkBoxCR.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLF
+            // 
+            this.checkBoxLF.AutoSize = true;
+            this.checkBoxLF.Location = new System.Drawing.Point(656, 326);
+            this.checkBoxLF.Name = "checkBoxLF";
+            this.checkBoxLF.Size = new System.Drawing.Size(38, 17);
+            this.checkBoxLF.TabIndex = 23;
+            this.checkBoxLF.Text = "LF";
+            this.checkBoxLF.UseVisualStyleBackColor = true;
+            // 
+            // textBoxTerminatorManualy
+            // 
+            this.textBoxTerminatorManualy.Location = new System.Drawing.Point(567, 382);
+            this.textBoxTerminatorManualy.Name = "textBoxTerminatorManualy";
+            this.textBoxTerminatorManualy.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTerminatorManualy.TabIndex = 24;
+            this.textBoxTerminatorManualy.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(564, 366);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(160, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Terminator ręczny (1 lub 2 znaki)";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxTerminatorManualy);
+            this.Controls.Add(this.checkBoxLF);
+            this.Controls.Add(this.checkBoxCR);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBoxStopBits);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxParityBits);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxDataBits);
             this.Controls.Add(this.buttonReceive);
             this.Controls.Add(this.buttonSend);
             this.Controls.Add(this.txtReceive);
@@ -173,6 +317,7 @@
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -193,6 +338,18 @@
         private System.Windows.Forms.RichTextBox txtReceive;
         private System.Windows.Forms.Button buttonSend;
         private System.Windows.Forms.Button buttonReceive;
+        private System.Windows.Forms.ComboBox comboBoxDataBits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxParityBits;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBoxStopBits;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.CheckBox checkBoxLF;
+        private System.Windows.Forms.CheckBox checkBoxCR;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxTerminatorManualy;
     }
 }
 
--- RS232/Form1.cs
+++ RS232/Form1.cs
@@ -23,50 +23,56 @@ namespace RS232
             int speedMin = 20;
             int speedMax = 115000;
             int dataNumber = 30;
-            Double number;
            
             //odczytanie dostępnych portów wraz z wpisanie ich do rozwijanej listy
             comboBoxPort.Items.AddRange(SerialPort.GetPortNames());
 
             int jump = (speedMax - speedMin) / dataNumber;
-            for (double i = 0; i < dataNumber; i++)
+            for (int i = 0; i < dataNumber; i++)
             {
-                comboBoxSpeed.Items.Add(Convert.ToString(i * jump + speedMin + " b/s"));
+                comboBoxSpeed.Items.Add(i * jump + speedMin);
             }
 
             //sortowanie wyswietlanych nazw dostępnych portów
             comboBoxPort.Sorted = true;     //true - sortuj, false - nie sortuj
-            comboBoxSpeed = true;
+            //comboBoxSpeed.Sorted = true;  //coś nie działa
 
             if (comboBoxPort.Items.Count > 0)
                 comboBoxPort.SelectedIndex = 0;
             if (comboBoxSpeed.Items.Count > 0)
                 comboBoxSpeed.SelectedIndex = 0;
+            comboBoxParityBits.SelectedIndex = 0;
+            comboBoxDataBits.SelectedIndex = 0;
+            comboBoxStopBits.SelectedIndex = 0;
 
             //aktywacja i deaktywacja odpowiednich kontrolek
             comboBoxPort.Enabled = true;   //lista z portami
             comboBoxSpeed.Enabled = true;   //lista z prędkością
+            comboBoxParityBits.Enabled = true;   //lista z kontrolą znaku
+            comboBoxDataBits.Enabled = true;   //lista z bitami pola danych
+            comboBoxStopBits.Enabled = true;   //lista z bitami stopu
             buttonConnect.Enabled = true;   //przycisk połącz
             buttonDisconnect.Enabled = false;   //przycisk rozłącz
-            buttonSend.Enabled = false;
-            buttonReceive.Enabled = false;
-              
-            
-
-
+            buttonSend.Enabled = false;     //przycisk wyślij
+            buttonReceive.Enabled = false;  //przycisk odbierz
         }
 
         private void buttonConnect_Click(object sender, EventArgs e)
         {
             //zmiana stanu przycisków po kliknięciu
-            buttonConnect.Enabled = false;   //przycisk połącz
+            buttonConnect.Enabled = false;      //przycisk połącz
             buttonDisconnect.Enabled = true;   //przycisk rozłącz
-            buttonSend.Enabled = true;
-            buttonReceive.Enabled = true;
+            buttonSend.Enabled = true;         //przycisk wyślij 
+            buttonReceive.Enabled = true;       //przycisk odbierz
             try
             {
+                serialPort.BaudRate = Convert.ToInt32(comboBoxSpeed.Text);  //konwersja i ustawienie prędkości transmisji
                 serialPort.PortName = comboBoxPort.Text;
+                serialPort.DataBits = Convert.ToInt32(comboBoxDataBits.Text);
+                serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), comboBoxStopBits.Text);
+                serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), comboBoxParityBits.Text);
                 serialPort.Open();
+
             }
             catch(Exception ex)
             {
@@ -81,7 +87,29 @@ namespace RS232
             {
                 if (serialPort.IsOpen)
                 {
-                    serialPort.WriteLine(txtMessage.Text + Environment.NewLine);
+                    //wysłanie bufora na łącze
+                    serialPort.Write(txtMessage.Text.ToString());
+
+                    //dopisanie terminatora do bufora
+
+                    if (textBoxTerminatorManualy.Text.Length == 0 || textBoxTerminatorManualy.Text.Length>2)
+                    {
+                        if (checkBoxCR.Checked)     //czy ma wysyłać polecenie powrotu karetki
+                        {
+                            //wysłanie polecenia powrotu
+                            serialPort.Write("\r");
+                        }
+                        if (checkBoxLF.Checked)     //czy ma wysyłać polecenie nowej lini
+                        {
+                            //wysłanie polecenia nowej lini
+                            serialPort.Write("\n");
+                        }
+                    }
+                    else
+                    {
+                        serialPort.Write(textBoxTerminatorManualy.Text.ToString());
+                    }
+                    
                     txtMessage.Clear();
                 }
             }
@@ -94,10 +122,10 @@ namespace RS232
         private void buttonDisconnect_Click(object sender, EventArgs e)
         {
             //zmiana stanu przycisków po kliknięciu
-            buttonConnect.Enabled = true;   //przycisk połącz
+            buttonConnect.Enabled = true;       //przycisk połącz
             buttonDisconnect.Enabled = false;   //przycisk rozłącz
-            buttonSend.Enabled = false;
-            buttonReceive.Enabled = false;
+            buttonSend.Enabled = false;         //przycisk wyślij 
+            buttonReceive.Enabled = false;      //przycisk odbierz
             try
             {
                 serialPort.Close();
@@ -127,6 +155,39 @@ namespace RS232
         {
             if (serialPort.IsOpen)
                 serialPort.Close();
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //sprawdzenie czy komponent gdzie wypisywane są odebrane dane jest w tym samym wątku co odbiór danych
+            if (txtReceive.InvokeRequired)
+            {
+                //utworzenie delegata (wskaźnika do mikro funkcji) metody do wpisywania danych w komponencie z bufora odbioru danych
+                Action act = () => txtReceive.Text += serialPort.ReadExisting();
+
+                //wykonanie delegata dla wątku głównego
+                Invoke(act);   //wywołanie delegata
+            }
+            else
+            {
+                //jeżeli jest w tym samym wątku przepisz normalnie dane z bufora do komponentu
+                txtReceive.Text += serialPort.ReadExisting();
+            }
+        }
+
+        private void comboBoxSpeed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxStopBits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
